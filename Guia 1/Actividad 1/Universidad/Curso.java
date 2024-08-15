39494d32c412d2961abc94e1cf2badb1713d7be6
Oscar Moreno <147674563+omoreno62465@users.noreply.github.com>
2024-08-15 18:26:57 -0500

    Add files via upload

---
+++ Guia 1/Actividad 1/Universidad/Curso.java
@@ -0,0 +1,15 @@
+public class Curso {
+
+    public Curso() {
+    }
+
+    public int codigo_numerico;
+
+    public String nombre;
+
+    public int cantidad_creditos;
+
+    public int numero_salon;
+
+    public String nombre_edificio;
+}
